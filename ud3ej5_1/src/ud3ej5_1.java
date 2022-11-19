27a31fdb581ab49bb34f3b634101a9f95a80eebb
M Latour C <manlatcov@alu.edu.gva.es>
2022-11-19 20:18:16 +0100

    First commit

---
+++ ud3ej5_1/src/ud3ej5_1.java
@@ -0,0 +1,19 @@
+
+
+
+public class ud3ej5_1 {
+    public static String veredicto(String valor1, String valor2) {
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
+            return "Poco tiempo de estudio. debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + ":");
+    }
+}
+
